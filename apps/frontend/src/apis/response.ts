a4e267e24960b0f0784243686a0bda5195a6ba85
guesung <gueit214@naver.com>
2025-12-25 15:29:20 +0900

    refactor: 타입 순서 변경

--- apps/frontend/src/apis/response.ts
+++ apps/frontend/src/apis/response.ts
@@ -1,5 +1,5 @@
 import { Customer, CustomerPurchase, PurchaseFrequency } from '@/types'
 
-export type PurchaseFrequencyResponse = PurchaseFrequency[]
 export type CustomersResponse = Customer[]
 export type CustomersPurchaseResponse = CustomerPurchase[]
+export type PurchaseFrequencyResponse = PurchaseFrequency[]
